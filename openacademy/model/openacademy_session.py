7b1a5cf088aa5842beffd25719ea0a1fa029345d
Gabriela Mogollón <tucorre@tudominio.com>
2016-11-21 18:48:33 +0000

    [REF] openacademy: Add domain or and ilike

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from openerp import fields, models
 
+
 class Session(models.Model):
     _name = 'openacademy.session'
 
@@ -8,7 +9,10 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
-    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    instructor_id = fields.Many2one('res.partner', string="Instructor",
+        domain=['|', ('instructor', '=', True),
+                     ('category_id.name', 'ilike', "Teacher")])
     course_id = fields.Many2one('openacademy.course',
-        ondelete='cascade', string="Course", required=True)
+                                ondelete='cascade', string="Course",
+                                required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
