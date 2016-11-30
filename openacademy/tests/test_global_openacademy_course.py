ed4add3969bd164745a1f4c4be00a0a073fbe99f
Gabriela Mogollón <tucorre@tudominio.com>
2016-11-30 19:27:25 +0000

    [REF] openacademy: Add duplicate test to openacademy course

--- openacademy/tests/test_global_openacademy_course.py
+++ openacademy/tests/test_global_openacademy_course.py
@@ -26,7 +26,7 @@ class GlobalTestOpenAcademyCourse(TransactionCase):
         
     # Method of test startswith 'def test_*(self):'
     @mute_logger('openerp.sql_db')
-    def test_01_same_name_description(self):
+    def test_10_same_name_description(self):
         """
         Test create a course with same name and description.
         to test constraint of name different to description.
@@ -37,3 +37,24 @@ class GlobalTestOpenAcademyCourse(TransactionCase):
                 ' constraint "openacademy_course_name_description_check"'):
             # create a course to raise error
             self.create_course('test', 'test', None)
+
+    @mute_logger('openerp.sql_db')
+    def test_20_two_course_same_name(self):
+        """
+        Test to create two course with same name
+        To raise constraint of unique name
+        """
+        new_id = self.create_course('test1', 'test_description', None)
+        print "new_id", new_id
+        with self.assertRaisesRegexp(IntegrityError,
+                'duplicate key value violates unique constraint'
+                ' "openacademy_course_name_unique"'): 
+            new_id2 = self.create_course('test1', 'test_description', None)
+
+    def test_15_duplicate_course(self):
+        """
+        Test to duplicate a course and check that works fine
+        """
+        course = self.env.ref('openacademy.course0')
+        course_id = course.copy()
+        print "course_id", course_id
